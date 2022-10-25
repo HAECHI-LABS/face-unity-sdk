0b3ddfa63f8d25111d58fe0faa3e9644f8c25495
zerofruit <14wnrkim@gmail.com>
2022-10-25 12:04:31 +0900

    fix: Add JsonProperty to data

--- Assets/haechi.face.unity.sdk/Runtime/Client/Face/FaceLoginResponse.cs
+++ Assets/haechi.face.unity.sdk/Runtime/Client/Face/FaceLoginResponse.cs
@@ -8,7 +8,10 @@ namespace haechi.face.unity.sdk.Runtime.Client.Face
     [Serializable]
     public class FaceLoginResponse
     {
+        [JsonProperty("faceUserId")]
         public string faceUserId;
+        
+        [JsonProperty("wallet")]
         public Wallet wallet;
         
         [JsonConstructor]
@@ -18,9 +21,16 @@ namespace haechi.face.unity.sdk.Runtime.Client.Face
         
         public class Wallet
         {
+            [JsonProperty("id")]
             public string Id;
+            
+            [JsonProperty("address")]
             public string Address;
+            
+            [JsonProperty("ecdsaPublicKey")]
             public string EcdsaPublicKey;
+            
+            [JsonProperty("eddsaPublicKey")]
             public string EddsaPublicKey;
         }
     }
