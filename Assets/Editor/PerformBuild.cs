d939b6983977ee287274c21014aa3c1e5889812e
Eddie Jeong <kunka8271@gmail.com>
2023-01-08 17:08:37 +0900

    chore: Add iOS build
    chore: Add automatic build feature  
    
    
    chore: Add android distribute
    
    
    chore: Update ruby version
    
    
    chore: Add gem lock platforms
    chore: Fix Install spelling
    chore: Add fastlane plugin
    chore: Downgrade ruby version
    fix: Set sudo
    chore: Add IOS distribution
    
    
    feat: Set build for all platform

--- Assets/Editor/PerformBuild.cs
+++ Assets/Editor/PerformBuild.cs
@@ -8,14 +8,14 @@ class PerformBuild
         buildPlayerOptions.options = BuildOptions.None;
         buildPlayerOptions.scenes = new[] { "Assets/haechi.face.unity.sdk/Samples/SampleDapp/Scenes/SampleScene.unity" };
         buildPlayerOptions.target = BuildTarget.Android;
-        buildPlayerOptions.locationPathName = "face-unity-sample.apk";
+        buildPlayerOptions.locationPathName = "Build/Android.apk";
         BuildPipeline.BuildPlayer(buildPlayerOptions); 
     }
     public static void IOS ()
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] { "Assets/haechi.face.unity.sdk/Samples/SampleDapp/Scenes/SampleScene.unity" };
-        buildPlayerOptions.locationPathName = "face-unity-sample";
+        buildPlayerOptions.locationPathName = "Build/ios";
         buildPlayerOptions.target = BuildTarget.iOS;
         BuildPipeline.BuildPlayer(buildPlayerOptions); 
     }
