0a2baa8654cb948cbb0ed7ef64f4493c86b8220e
rasplarry <lawliet0521@gmail.com>
2023-03-13 14:23:02 +0900

    test: Add meverse, bora network tests

--- Assets/haechi.face.unity.sdk/Tests/Runtime/Type/NeworkTests.cs
+++ Assets/haechi.face.unity.sdk/Tests/Runtime/Type/NeworkTests.cs
@@ -10,23 +10,33 @@ namespace haechi.face.unity.sdk.Tests.Runtime.Type
         {
             Assert.AreEqual(BlockchainNetwork.GOERLI, BlockchainNetworks.GetNetwork("ETHEREUM", "StageTest"));
             Assert.AreEqual(BlockchainNetwork.ETHEREUM, BlockchainNetworks.GetNetwork("ETHEREUM", "StageMainnet"));
-            Assert.AreEqual(BlockchainNetwork.GOERLI, BlockchainNetworks.GetNetwork("ETHEREUM", "ProdTest"));
-            Assert.AreEqual(BlockchainNetwork.ETHEREUM, BlockchainNetworks.GetNetwork("ETHEREUM", "ProdMainnet"));
+            Assert.AreEqual(BlockchainNetwork.GOERLI, BlockchainNetworks.GetNetwork("ETHEREUM", "Testnet"));
+            Assert.AreEqual(BlockchainNetwork.ETHEREUM, BlockchainNetworks.GetNetwork("ETHEREUM", "Mainnet"));
             
             Assert.AreEqual(BlockchainNetwork.MUMBAI, BlockchainNetworks.GetNetwork("POLYGON", "StageTest"));
             Assert.AreEqual(BlockchainNetwork.POLYGON, BlockchainNetworks.GetNetwork("POLYGON", "StageMainnet"));
-            Assert.AreEqual(BlockchainNetwork.MUMBAI, BlockchainNetworks.GetNetwork("POLYGON", "ProdTest"));
-            Assert.AreEqual(BlockchainNetwork.POLYGON, BlockchainNetworks.GetNetwork("POLYGON", "ProdMainnet"));
+            Assert.AreEqual(BlockchainNetwork.MUMBAI, BlockchainNetworks.GetNetwork("POLYGON", "Testnet"));
+            Assert.AreEqual(BlockchainNetwork.POLYGON, BlockchainNetworks.GetNetwork("POLYGON", "Mainnet"));
             
             Assert.AreEqual(BlockchainNetwork.BNB_SMART_CHAIN_TESTNET, BlockchainNetworks.GetNetwork("BNB_SMART_CHAIN", "StageTest"));
             Assert.AreEqual(BlockchainNetwork.BNB_SMART_CHAIN, BlockchainNetworks.GetNetwork("BNB_SMART_CHAIN", "StageMainnet"));
-            Assert.AreEqual(BlockchainNetwork.BNB_SMART_CHAIN_TESTNET, BlockchainNetworks.GetNetwork("BNB_SMART_CHAIN", "ProdTest"));
-            Assert.AreEqual(BlockchainNetwork.BNB_SMART_CHAIN, BlockchainNetworks.GetNetwork("BNB_SMART_CHAIN", "ProdMainnet"));
+            Assert.AreEqual(BlockchainNetwork.BNB_SMART_CHAIN_TESTNET, BlockchainNetworks.GetNetwork("BNB_SMART_CHAIN", "Testnet"));
+            Assert.AreEqual(BlockchainNetwork.BNB_SMART_CHAIN, BlockchainNetworks.GetNetwork("BNB_SMART_CHAIN", "Mainnet"));
             
             Assert.AreEqual(BlockchainNetwork.BAOBAB, BlockchainNetworks.GetNetwork("KLAYTN", "StageTest"));
             Assert.AreEqual(BlockchainNetwork.KLAYTN, BlockchainNetworks.GetNetwork("KLAYTN", "StageMainnet"));
-            Assert.AreEqual(BlockchainNetwork.BAOBAB, BlockchainNetworks.GetNetwork("KLAYTN", "ProdTest"));
-            Assert.AreEqual(BlockchainNetwork.KLAYTN, BlockchainNetworks.GetNetwork("KLAYTN", "ProdMainnet"));
+            Assert.AreEqual(BlockchainNetwork.BAOBAB, BlockchainNetworks.GetNetwork("KLAYTN", "Testnet"));
+            Assert.AreEqual(BlockchainNetwork.KLAYTN, BlockchainNetworks.GetNetwork("KLAYTN", "Mainnet"));
+            
+            Assert.AreEqual(BlockchainNetwork.MEVERSE_TESTNET, BlockchainNetworks.GetNetwork("MEVERSE", "StageTest"));
+            Assert.AreEqual(BlockchainNetwork.MEVERSE, BlockchainNetworks.GetNetwork("MEVERSE", "StageMainnet"));
+            Assert.AreEqual(BlockchainNetwork.MEVERSE_TESTNET, BlockchainNetworks.GetNetwork("MEVERSE", "Testnet"));
+            Assert.AreEqual(BlockchainNetwork.MEVERSE, BlockchainNetworks.GetNetwork("MEVERSE", "Mainnet"));
+            
+            Assert.AreEqual(BlockchainNetwork.BORA_TESTNET, BlockchainNetworks.GetNetwork("BORA", "StageTest"));
+            Assert.AreEqual(BlockchainNetwork.BORA, BlockchainNetworks.GetNetwork("BORA", "StageMainnet"));
+            Assert.AreEqual(BlockchainNetwork.BORA_TESTNET, BlockchainNetworks.GetNetwork("BORA", "Testnet"));
+            Assert.AreEqual(BlockchainNetwork.BORA, BlockchainNetworks.GetNetwork("BORA", "Mainnet"));
         }
     }
 }
