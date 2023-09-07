fd2d183c696f588a88632e02a355610a8f9efb4c
borre <borre@haechi.io>
2023-09-07 16:32:51 +0900

    chore: Do not build unitypackage

--- scripts/copyAndroidLibraries.mjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zx
-
-const fromDirectory = "android-dependency-generator/Assets/Plugins/Android";
-const toDirectory = "haechi.face.unity.sdk/Plugins/Android"
-await $`ls ${toDirectory}`
-await $`cp -f ${fromDirectory}/* ${toDirectory}/`
-await $`ls ${toDirectory}`
-
