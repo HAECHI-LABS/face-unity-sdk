1a89594041ceb6b7af2f02f8e14f7438def58475
borre <borre@haechi.io>
2024-10-08 10:04:41 +0900

    Add dependency installation to BuildUnityPackages

--- unitypackage-deployer/Assets/Editor/BuildUnityPackages.cs
+++ unitypackage-deployer/Assets/Editor/BuildUnityPackages.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class BuildUnityPackages : MonoBehaviour
@@ -21,11 +24,48 @@ public class BuildUnityPackages : MonoBehaviour
             Debug.Log("Created ./Assets/HaechiLabs");
         }
 
-        FileUtil.CopyFileOrDirectory("../haechi.face.unity.sdk", "./Assets/HaechiLabs/haechi.face.unity.sdk");
-        Debug.Log("Copied ../haechi.face.unity.sdk to ./Assets/HaechiLabs/haechi.face.unity.sdk");
+        // read ../haechi.face.unity.sdk/package.json
+        string packageJson = File.ReadAllText("../haechi.face.unity.sdk/package.json");
+        // read depedencies
+        // deserialize packagejson and read dependencies using SimpleJSONUnity
+        var packageJsonObj = SimpleJSON.JSON.Parse(packageJson);
+        var dependencies = packageJsonObj["dependencies"];
+        Debug.Log("Dependencies: " + dependencies.AsObject + " " + dependencies.Count);
 
-        Debug.Log("Exporting package...");
-        UnityEditor.AssetDatabase.ExportPackage("./Assets/HaechiLabs/haechi.face.unity.sdk", "../haechi.face.unity.sdk.unitypackage");
-        Debug.Log("Exported package at ../haechi.face.unity.sdk.unitypackage");
+        foreach (var dependency in dependencies.AsObject)
+        {
+            var kv = (KeyValuePair<string, SimpleJSON.JSONNode>)dependency;
+            Debug.Log("Dependency: " + kv.Key + " " + kv.Value);
+        }
+
+        // install dependencies
+        foreach (var dependency in dependencies.AsObject)
+        {
+            var kv = (KeyValuePair<string, SimpleJSON.JSONNode>)dependency;
+            Debug.Log("Install Dependency: " + kv.Key + " " + kv.Value.Value + " " + $"{kv.Key}@{kv.Value.Value}");
+            AddRequest addRequest = Client.Add($"{kv.Key}@{kv.Value.Value}");
+            while (!addRequest.IsCompleted)
+            {
+                Debug.Log("Waiting for add request to complete...");
+                System.Threading.Thread.Sleep(1000);
+            }
+            if (addRequest.Status == StatusCode.Success)
+            {
+                Debug.Log("Installed " + kv.Key + " " + kv.Value);
+            }
+            else
+            {
+                Debug.LogError("Failed to install " + kv.Key + " " + kv.Value + " " + addRequest.Error.message);
+                Debug.LogError(addRequest.Error);
+            }
+        }
+
+
+        // FileUtil.CopyFileOrDirectory("../haechi.face.unity.sdk", "./Assets/HaechiLabs/haechi.face.unity.sdk");
+        // Debug.Log("Copied ../haechi.face.unity.sdk to ./Assets/HaechiLabs/haechi.face.unity.sdk");
+
+        // Debug.Log("Exporting package...");
+        // UnityEditor.AssetDatabase.ExportPackage("./Assets/HaechiLabs/haechi.face.unity.sdk", "../haechi.face.unity.sdk.unitypackage");
+        // Debug.Log("Exported package at ../haechi.face.unity.sdk.unitypackage");
     }
 }
